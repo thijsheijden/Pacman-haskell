488219a593b0f951bdf05b3a90ebbdf72e4b5d26
thijsheijden <hi@thijsheijden.nl>
2020-10-24 20:25:09 +0200

    Added basic map. Changed wall images. Rendering map now works with most parts.

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,13 @@
 module Main where
 
-import Controller
-import Model
-import View
-import System.Random
+import Controller ( input, step )
+import Model ( initialState )
+import View ( view )
+import System.Random ( newStdGen )
 
 import Graphics.Gloss.Interface.IO.Game
-import Graphics.Gloss
+    ( black, Display(InWindow), playIO )
+import Graphics.Gloss ( black, loadBMP, Display(InWindow) )
 
 main :: IO ()
 main = do
@@ -14,6 +15,15 @@ main = do
   -- Loading all images
   horizontal <- loadBMP "img/blocks/lines.bmp"
   vertical <- loadBMP "img/blocks/lines vertical.bmp"
+  rightCorner <- loadBMP "img/blocks/left top corner.bmp"
+  downCorner <- loadBMP "img/blocks/right top corner.bmp"
+  leftCorner <- loadBMP "img/blocks/right bottom corner.bmp"
+  upCorner <- loadBMP "img/blocks/left bottom corner.bmp"
+  upConnector <- loadBMP "img/blocks/connector top.bmp"
+  topRounded <- loadBMP "img/blocks/lines vertical top rounded.bmp"
+  bottomRounded <- loadBMP "img/blocks/lines vertical bottom rounded.bmp"
+  rightRounded <- loadBMP "img/blocks/lines horizontal right rounded.bmp"
+  leftRounded <- loadBMP "img/blocks/lines horizontal left rounded.bmp"
   
   stdGen <- newStdGen
   map <- readFile "map.txt"
@@ -21,7 +31,7 @@ main = do
   playIO (InWindow "Pacman" (1280, 720) (0, 0)) -- Or FullScreen
               black            -- Background color
               30               -- Frames per second
-              (initialState stdGen map [horizontal, vertical])     -- Initial state
+              (initialState stdGen map [horizontal, vertical, rightCorner, downCorner, leftCorner, upCorner, upConnector, topRounded, bottomRounded, rightRounded, leftRounded])     -- Initial state
               view             -- View function
               input            -- Event function
               step             -- Step function
--- src/Model.hs
+++ src/Model.hs
@@ -6,7 +6,19 @@ import System.Random
 import Graphics.Gloss
 
 initialState :: StdGen -> String -> [Picture] -> GameState
-initialState stdGen map pics = GameState { stdGen = stdGen, board = createBoard map, pics = pics }
+initialState stdGen map pics = GameState {  stdGen = stdGen, 
+                                            board = board, 
+                                            pics = pics, 
+                                            gridSize = gridSize, 
+                                            numberOfColumns = numberOfColumns, 
+                                            numberOfRows = numberOfRows
+                                          }
+  where
+    board = createBoard map
+    numberOfColumns = fromIntegral $ length $ head board
+    numberOfRows = fromIntegral $ length board
+    gridSize = calculateGridSize board numberOfColumns numberOfRows
+
 
 data GameState = GameState {  player        :: Player,
                               board         :: Board,
@@ -50,7 +62,7 @@ data MovementDirection = Up | Down | Left | Right | None
 data Field = Pacdot | Energizer | Cherry | Empty | RightCorner | DownCorner | LeftCorner | UpCorner 
             | Horizontal | Vertical | UpConnector | DownConnector 
             | LeftConnector | RightConnector | AllConnector | LeftRounded 
-            | RightRounder | TopRounded | BottomRounded
+            | RightRounded | TopRounded | BottomRounded
 type Row = [Field]
 type Board = [Row]
 
@@ -61,13 +73,22 @@ createBoard t = map (rowToFields . words) (lines t)
 rowToFields :: [String] -> Row
 rowToFields = map stringToField
   where stringToField :: String -> Field
-        stringToField "b" = Pacdot
+        stringToField "b" = Empty
         stringToField "uc" = UpCorner
         stringToField "dc" = DownCorner
         stringToField "lc" = LeftCorner
         stringToField "rc" = RightCorner
         stringToField "l" = Horizontal
         stringToField "v" = Vertical
+        stringToField "uo" = UpConnector
+        stringToField "tr" = TopRounded
+        stringToField "br" = BottomRounded
+        stringToField "rr" = RightRounded
+        stringToField "lr" = LeftRounded
+
+calculateGridSize :: Board -> Float -> Float -> Float
+calculateGridSize board numberOfColumns numberOfRows  | 1280 / numberOfColumns < 720 / numberOfRows = 1280 / numberOfColumns
+                                                      | otherwise = 720 / numberOfRows
 
 -- Renderable typeclass
 -- All items which can be rendered should implement this
@@ -75,17 +96,27 @@ class Renderable a where
   render :: GameState -> a -> Float -> Float -> Picture
 
 instance Renderable Field where
-  render gstate Pacdot = translatePicture gstate (circleSolid (gridSize gstate))
-  render gstate Horizontal = translatePicture gstate (head $ pics gstate)
-  render gstate Vertical = translatePicture gstate (pics gstate !! 1)
-  render gstate _ = translatePicture gstate (pics gstate !! 1)
-  -- render gstate RightCorner = translatePicture gstate (pics gstate !! 2)
-  -- render gstate DownCorner = translatePicture gstate (pics gstate !! 3)
-  -- render gstate LeftCorner = translatePicture gstate (pics gstate !! 4)
-  -- render gstate UpCorner = translatePicture gstate (pics gstate !! 5)
+  render gstate Horizontal = translatePicture gstate (scalePicture gstate (head $ pics gstate))
+  render gstate Vertical = translatePicture gstate (scalePicture gstate (pics gstate !! 1))
+  render gstate RightCorner = translatePicture gstate (scalePicture gstate (pics gstate !! 2))
+  render gstate DownCorner = translatePicture gstate (scalePicture gstate (pics gstate !! 3))
+  render gstate LeftCorner = translatePicture gstate (scalePicture gstate (pics gstate !! 4))
+  render gstate UpCorner = translatePicture gstate (scalePicture gstate (pics gstate !! 5))
+  render gstate UpConnector = translatePicture gstate (scalePicture gstate (pics gstate !! 6))
+  render gstate TopRounded = translatePicture gstate (scalePicture gstate (pics gstate !! 7))
+  render gstate BottomRounded = translatePicture gstate (scalePicture gstate (pics gstate !! 8))
+  render gstate RightRounded = translatePicture gstate (scalePicture gstate (pics gstate !! 9))
+  render gstate LeftRounded = translatePicture gstate (scalePicture gstate (pics gstate !! 10))
+  render gstate Empty = translatePicture gstate (color white $ rectangleSolid 10 10)
+  render gstate _ = translatePicture gstate (color white $ rectangleSolid (gridSize gstate) (gridSize gstate))
 
 
--- Function to translate a picture to a location on the screen from x,y position
+-- Function to scale a picture to a certain size
+scalePicture :: GameState -> Picture -> Picture
+scalePicture gstate = scale (gSize / 100) (gSize / 100)
+  where gSize = gridSize gstate
+
+-- Function to translate a picture to a location on the screen at a certain x and y index in the grid world
 translatePicture :: GameState -> Picture -> Float -> Float -> Picture
 translatePicture gstate p x y = Translate ((-(nColumns * gSize) * 0.5) + 0.5 * gSize + x * gSize) (((nRows * gSize) * 0.5) - 0.5 * gSize - y * gSize) p
   where gSize = gridSize gstate
